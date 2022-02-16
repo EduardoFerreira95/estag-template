58781600ea7776bdc883bfacc547d79168a6b494
jackmcosta32 <42323045+jackmcosta32@users.noreply.github.com>
2022-02-16 19:33:56 -0300

    feat: jest unit tests (#1)

---
+++ tests/example.spec.ts
@@ -0,0 +1,10 @@
+/// <reference types="jest" />
+
+describe('Test Example', () => {
+  it('should be able to correctly match two variables', () => {
+    const values = [1,2,3,4,5,6,7,8,9,10]
+    const even = values.filter(element => !Boolean(element % 2))
+
+    expect(even).toStrictEqual([2, 4, 6, 8, 10])
+  })
+})
