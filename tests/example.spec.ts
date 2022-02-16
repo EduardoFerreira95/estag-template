f977cf64d240ac7011fe79f5cecd23695f8f519f
joaocosta <joaomcosta32@gmail.com>
2022-02-16 18:15:20 -0300

    feat: eslint

--- tests/example.spec.ts
+++ tests/example.spec.ts
@@ -3,7 +3,7 @@
 describe('Test Example', () => {
   it('should be able to correctly match two variables', () => {
     const values = [1,2,3,4,5,6,7,8,9,10]
-    const even = values.filter(element => !Boolean(element % 2))
+    const even = values.filter(element => !(element % 2))
 
     expect(even).toStrictEqual([2, 4, 6, 8, 10])
   })
