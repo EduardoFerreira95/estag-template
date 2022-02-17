ac456d276de4cdf6264c076766483906854fbd89
Eduardo Ferreira <eduardoferreira@wine.com.br>
2022-02-17 09:09:21 -0300

    improvement: customer services

---
+++ App/Services/CustomerService.ts
@@ -0,0 +1,8 @@
+import { HttpClientModel } from "../Api/model";
+
+class CustomerServices {
+  constructor(private readonly httpClient: HttpClientModel) {}
+  public async create() {
+    // TODO
+  }
+}
