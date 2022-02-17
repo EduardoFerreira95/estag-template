89822d1628a200bbec82547689a463ca044dec56
Eduardo Ferreira <eduardoferreira@wine.com.br>
2022-02-17 10:54:20 -0300

    improvement: rm api temporally

--- App/Api/model.ts
+++
@@ -1,6 +0,0 @@
-export type HttpClientModel = {
-  get: <T extends unknown, R extends unknown>(baseUrl: string, body?: T) => Promise<R>;
-  post: <T extends unknown, R extends unknown>(baseUrl: string, body?: T) => Promise<R>;
-  put: <T extends unknown, R extends unknown>(baseUrl: string, body?: T) => Promise<R>;
-  delete: <T extends unknown, R extends unknown>(baseUrl: string, body?: T) => Promise<R>;
-}
